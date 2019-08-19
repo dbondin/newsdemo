00634c194465c730ad19364854acbae84a877284
Dmitry Bondin <Dmitry.Bondin@stm-labs.ru>
2019-08-19 14:37:13 +0300

    add newsdemo-server template project

---
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/NewsdemoServerApplication.java
@@ -0,0 +1,13 @@
+package ru.stm.newsdemo.newsdemoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsdemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsdemoServerApplication.class, args);
+	}
+
+}
