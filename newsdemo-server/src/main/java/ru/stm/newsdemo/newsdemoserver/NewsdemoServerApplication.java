6897acbc8bf905be157df63118f24bdf500a8de4
Dmitry Bondin <Dmitry.Bondin@stm-labs.ru>
2019-08-22 11:02:52 +0300

    issue/9 move changes by AlexandraM to this branch

--- newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/NewsdemoServerApplication.java
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/NewsdemoServerApplication.java
@@ -2,8 +2,9 @@ package ru.stm.newsdemo.newsdemoserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@ComponentScan
 public class NewsdemoServerApplication {//new commit
 
 	public static void main(String[] args) {
