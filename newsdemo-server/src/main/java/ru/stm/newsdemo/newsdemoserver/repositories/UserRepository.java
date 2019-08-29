2b50d87501392db8a92dce99aec4eb448cb532e8
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-08-29 15:04:15 +0300

    issue/16

--- newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/repositories/UserRepository.java
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/repositories/UserRepository.java
@@ -10,5 +10,5 @@ import ru.stm.newsdemo.newsdemoserver.domain.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-	Optional<User> findByUsername(String username);
+	User findByUsername(String username);
 }
