43b29771f7b2843514b641c29db6258b265664b8
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-09-02 19:22:30 +0300

    issue/16

--- newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/NewsdemoServerApplication.java
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/NewsdemoServerApplication.java
@@ -2,13 +2,10 @@ package ru.stm.newsdemo.newsdemoserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-@Configuration
-@ComponentScan
+
+
 @SpringBootApplication
-public class NewsdemoServerApplication extends SpringBootServletInitializer{
+public class NewsdemoServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewsdemoServerApplication.class, args);
 
