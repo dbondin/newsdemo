8b11bfa5beb69db42bcb8941bedc5fef7db2811c
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-09-02 19:26:28 +0300

    issue/16

--- newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/service/UserService.java
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/service/UserService.java
@@ -1,18 +1,21 @@
 package ru.stm.newsdemo.newsdemoserver.service;
 
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ru.stm.newsdemo.newsdemoserver.domain.Role;
 import ru.stm.newsdemo.newsdemoserver.domain.User;
 import ru.stm.newsdemo.newsdemoserver.repositories.UserRepository;
 
 @Service
 public class UserService {
-
+private Set<Role>roles=new HashSet<>();
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -27,7 +30,7 @@ public class UserService {
 	}
 	
 	@Transactional(readOnly = true)
-	public User findByUsername(final String username) {
+	public Optional<User> findByUsername(final String username) {
 		return userRepository.findByUsername(username);
 	}
 	
@@ -35,6 +38,16 @@ public class UserService {
 	public Optional<User> findById(final Long id) {
 		return userRepository.findById(id);
 	}
+//public void addRole(Role role) {
+//		roles.add( role );
+//		role.getUsers().add( this );
+//	}
 
+	//public void removeRole(Role role) {
+	//	roles.remove( role );
+	//	role.getUsers().remove( this );
+	//	
+		
+	//}
 
 }
