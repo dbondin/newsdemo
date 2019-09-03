6cde0d8db9884225645868b6ad2e7c79a470bf25
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-09-03 07:11:11 +0300

    issue/16

--- newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/service/UserService.java
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/service/UserService.java
@@ -1,39 +1,35 @@
 package ru.stm.newsdemo.newsdemoserver.service;
 
-
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ru.stm.newsdemo.newsdemoserver.domain.Role;
 import ru.stm.newsdemo.newsdemoserver.domain.User;
 import ru.stm.newsdemo.newsdemoserver.repositories.UserRepository;
 
 @Service
 public class UserService {
-private Set<Role>roles=new HashSet<>();
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Transactional(readOnly = false)
 	public User save(final User user) {
 		return userRepository.save(user);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Iterable<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Optional<User> findByUsername(final String username) {
 		return userRepository.findByUsername(username);
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Optional<User> findById(final Long id) {
 		return userRepository.findById(id);
@@ -43,11 +39,11 @@ private Set<Role>roles=new HashSet<>();
 //		role.getUsers().add( this );
 //	}
 
-	//public void removeRole(Role role) {
-	//	roles.remove( role );
-	//	role.getUsers().remove( this );
-	//	
-		
-	//}
+	// public void removeRole(Role role) {
+	// roles.remove( role );
+	// role.getUsers().remove( this );
+	//
+
+	// }
 
 }
