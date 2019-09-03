a3b1923b5282fb3e8203545af4ab96287c71468b
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-09-03 07:05:19 +0300

    issue/16

--- newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/NewsdemoServerApplication.java
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/NewsdemoServerApplication.java
@@ -3,7 +3,6 @@ package ru.stm.newsdemo.newsdemoserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class NewsdemoServerApplication {
 	public static void main(String[] args) {
