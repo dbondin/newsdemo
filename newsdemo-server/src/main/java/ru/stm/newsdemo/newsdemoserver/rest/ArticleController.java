d15038a6fb7518220621887235e28b495006376b
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-09-08 22:11:01 +0300

    issue/20

--- newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/rest/ArticleController.java
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/rest/ArticleController.java
@@ -4,21 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import ru.stm.newsdemo.newsdemoserver.domain.Article;
+import ru.stm.newsdemo.newsdemoserver.domain.Role;
+import ru.stm.newsdemo.newsdemoserver.domain.User;
 import ru.stm.newsdemo.newsdemoserver.rest.dto.DtoArticle;
 import ru.stm.newsdemo.newsdemoserver.rest.dto.DtoUser;
+import ru.stm.newsdemo.newsdemoserver.service.ArticleService;
 
 @RestController
 public class ArticleController {
-
-	@RequestMapping(method = RequestMethod.GET, path = "/article/last")
+@Autowired
+ArticleService articleService;
+	/*@RequestMapping(method = RequestMethod.GET, path = "/article/last")
 	public ResponseEntity<List<DtoArticle>> getLastArticles() {
 		List<DtoArticle> result = new ArrayList<>();
 		for (int i = 0; i < 3; ++i) {
@@ -26,5 +34,10 @@ public class ArticleController {
 					new DtoUser(i, "Автор тестовой статьи" + i), new Date()));
 		}
 		return new ResponseEntity<List<DtoArticle>>(result, HttpStatus.OK);
+	}*/
+	
+	@RequestMapping(method=RequestMethod.GET,path="/article/last")
+	public ResponseEntity<Iterable<Article>> pushArticle() {
+		return new ResponseEntity<Iterable<Article>>(articleService.findAll(),HttpStatus.OK);
 	}
 }
