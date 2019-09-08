1ca5621c1b9b34d25d57f568be87f8aba829853d
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-09-08 22:12:37 +0300

    issue/20

---
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/domain/Article.java
@@ -0,0 +1,76 @@
+package ru.stm.newsdemo.newsdemoserver.domain;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ND_ARTICLE")
+public class Article {
+	@Id
+	@Column(name = "ID", nullable = false, unique = true)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ArticleIdSequenceGenerator")
+	@SequenceGenerator(sequenceName = "ND_ARTICLE_ID_SEQ", name = "ArticleIdSequenceGenerator", allocationSize = 1)
+	private Long id;
+	@Column(name = "TITLE", nullable = false, unique = true)
+	private String title;
+	@Column(name = "CONTENT", nullable = false, unique = false)
+	private String content;
+	@Column(name = "POSTING_DATE", nullable = false, unique = false, columnDefinition="DATE")
+
+	private Date posting_date;
+	@ManyToMany(mappedBy = "articles")
+	private Set<User> users=new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getPosting_date() {
+		return posting_date;
+	}
+
+	public void setPosting_date(Date posting_date) {
+		this.posting_date = posting_date;
+	}
+
+	public Article(String title, String content, Date posting_date, Set<User> users) {
+		this.title = title;
+		this.content = content;
+		this.posting_date = posting_date;
+		this.users = users;
+
+	}
+
+	public Article(String title, String content, Date posting_date) {
+		this.title = title;
+		this.content = content;
+		this.posting_date = posting_date;
+
+	}
+
+	public Article() {
+	}
+}
