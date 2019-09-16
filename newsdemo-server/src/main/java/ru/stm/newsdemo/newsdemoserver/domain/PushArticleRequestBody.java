d7f7fcb7adc735c983912513045efcc0ca112b58
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-09-16 17:12:34 +0300

    issue/20

---
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/domain/PushArticleRequestBody.java
@@ -0,0 +1,22 @@
+package ru.stm.newsdemo.newsdemoserver.domain;
+
+public class PushArticleRequestBody {
+private String title;
+private String content;
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getContent() {
+	return content;
+}
+public void setContent(String content) {
+	this.content = content;
+}
+public PushArticleRequestBody(String title, String content) {
+	this.title=title;
+	this.content=content;
+}
+}
