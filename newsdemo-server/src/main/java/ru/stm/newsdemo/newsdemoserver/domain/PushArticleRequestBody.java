07a615472500a69460626949921651c9c301e1e5
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-10-07 08:28:56 +0300

    issue/22

--- newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/domain/PushArticleRequestBody.java
+++ newsdemo-server/src/main/java/ru/stm/newsdemo/newsdemoserver/domain/PushArticleRequestBody.java
@@ -1,9 +1,13 @@
 package ru.stm.newsdemo.newsdemoserver.domain;
 
+import java.util.Date;
+
 public class PushArticleRequestBody {
-	
+
 	private String title;
 	private String content;
+	private Date date;
+	private User author;
 
 	public String getTitle() {
 		return title;
@@ -21,8 +25,26 @@ public class PushArticleRequestBody {
 		this.content = content;
 	}
 
-	public PushArticleRequestBody(String title, String content) {
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public PushArticleRequestBody(String title, String content,Date date, User author) {
 		this.title = title;
 		this.content = content;
+		this.date=date;
+		this.author=author;
 	}
 }
