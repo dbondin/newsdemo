fa5b9bc28a00d948ee415fd84fa88b2485e3efdb
Aleksandra Moiseenko <vip.smoiseenko@mail.ru>
2019-10-07 08:47:40 +0300

    issue/24

--- newsdemo-server/src/main/resources/static/js/lastArticles.js
+++ newsdemo-server/src/main/resources/static/js/lastArticles.js
@@ -6,8 +6,10 @@ var lastArticles = new Vue({
 		  '<div>'
 		+ ' <h1>Последние статьи</h1>'
 		+ ' <div v-for="a in articles">'
+		+ '  <div>{{ a.date }}</div>'
+		+ '  <div>{{ a.author.name }}</div>'
 		+ '  <div>{{ a.title }}</div>'
-		+ '  <div>{{ a.content }}</div>'
+		+ '  <div>{{ a.content.slice(0.50) }}</div>'
 		+ ' </div>'
 		+ '</div>'
 		,
